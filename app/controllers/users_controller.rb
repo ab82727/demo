2c09fd769b1afefbf93a89a5c0508611db9b2434
ab82727 <ab82727@yahoo.com.tw>
2015-08-04 02:19:11 +0800

    seco

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  authorize_resource :user
-  
+  authorize_resource  
   def test
     @users=User.all
   end
